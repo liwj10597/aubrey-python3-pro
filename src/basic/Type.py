4bb3387b749c6e419cc318c0b72f947d1f706ffb
李卫军 <aubrey@jollycorp.com>
2019-03-12 09:28:08 +0800

    数据类型

--- src/basic/Type.py
+++ src/basic/Type.py
@@ -42,3 +42,31 @@ print('------------------------------')
 
 print('hello\njollychic')      # 使用反斜杠(\)+n转义特殊字符
 print(r'hello\njollychic')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+
+#不换行输出
+print("li", end="")
+print("wei", end="")
+
+#查看变量数据类型
+print(type(str))
+
+#用isinstance判断数据类型
+id = 1001
+if isinstance(id, int):
+    print('id is int')
+else:
+    print("id is not int")
+
+#type不会认为子类是一种父类
+#isinstance会认为子类是一种父类
+
+#列表。列表中的元素类型可以不相同
+list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
+tinylist = [123, 'runoob']
+
+print (list)            # 输出完整列表
+print (list[0])         # 输出列表第一个元素
+print (list[1:3])       # 从第二个开始输出到第三个元素
+print (list[2:])        # 输出从第三个元素开始的所有元素
+print (tinylist * 2)    # 输出两次列表
+print (list + tinylist) # 连接列表
