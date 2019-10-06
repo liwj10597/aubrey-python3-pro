6cd4c9ed8b73615d1180e47a3f7e685f5ea8207b
aubrey@jollycorp.com <aubrey@jollycorp.com>
2019-10-06 16:26:16 +0800

    update

---
+++ src/common/common_func.py
@@ -0,0 +1,6 @@
+
+
+def sum_common(arg1, arg2):
+    return arg1 + arg2
+
+
--- src/senior/Models.py
+++ src/senior/Models.py
@@ -3,6 +3,7 @@
 
 import sys
 from src.basic import Func
+from src.common.common_func import sum_common
 
 print('命令行参数如下:')
 for i in sys.argv:
@@ -11,3 +12,7 @@ for i in sys.argv:
 print('\n\nPython 路径为：', sys.path, '\n')
 
 Func.print_param2(70, a=50, b=60)
+
+print(sum_common(2, 4))
+
+
