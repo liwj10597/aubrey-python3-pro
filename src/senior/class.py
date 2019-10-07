6c39ad2802971e07ee8e1b701c355229a060ae00
aubrey@jollycorp.com <aubrey@jollycorp.com>
2019-10-07 16:03:12 +0800

    update

--- src/senior/class.py
+++ src/senior/class.py
@@ -30,3 +30,29 @@ class student(people):
 
 s = student('ken', 10, 60, 3)
 s.speak()
+
+
+# 多重继承
+# 另一个类，多重继承之前的准备
+class speaker():
+    topic = ''
+    name = ''
+
+    def __init__(self, n, t):
+        self.name = n
+        self.topic = t
+
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s" % (self.name, self.topic))
+
+
+class sample(speaker, student):
+    a = ''
+
+    def __init__(self, n, a, w, g, t):
+        student.__init__(self, n, a, w, g)
+        speaker.__init__(self, n, t)
+
+
+test = sample("Tim", 25, 80, 4, "Python")
+test.speak()  # 方法名同，默认调用的是在括号中排前地父类的方法
